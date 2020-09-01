6f6fc6545351122894f59d8be90b82279414aa8e
ChanIk Kwon <39299919+lesslate@users.noreply.github.com>
2020-09-02 00:08:08 +0900

    Update user.service.js

--- app-services/user.service.js
+++ app-services/user.service.js
@@ -1,4 +1,5 @@
-﻿(function () {
+﻿(function ()
+{
     'use strict';
 
     angular
@@ -6,7 +7,8 @@
         .factory('UserService', UserService);
 
     UserService.$inject = ['$http'];
-    function UserService($http) {
+    function UserService($http)
+    {
         var service = {};
 
         service.GetAll = GetAll;
@@ -18,38 +20,47 @@
 
         return service;
 
-        function GetAll() {
+        function GetAll()
+        {
             return $http.get('/api/users').then(handleSuccess, handleError('Error getting all users'));
         }
 
-        function GetById(id) {
+        function GetById(id)
+        {
             return $http.get('/api/users/' + id).then(handleSuccess, handleError('Error getting user by id'));
         }
 
-        function GetByUsername(username) {
+        function GetByUsername(username)
+        {
             return $http.get('/api/users/' + username).then(handleSuccess, handleError('Error getting user by username'));
         }
 
-        function Create(user) {
-            return $http.post('/api/users', user).then(handleSuccess, handleError('Error creating user'));
+        function Create(user)
+        {
+            return $http.post('/api/users', user).then(handleSuccess, handleError('Error creating user')); // ID 생성
         }
 
-        function Update(user) {
+        function Update(user)
+        {
             return $http.put('/api/users/' + user.id, user).then(handleSuccess, handleError('Error updating user'));
         }
 
-        function Delete(id) {
+        function Delete(id)
+        {
             return $http.delete('/api/users/' + id).then(handleSuccess, handleError('Error deleting user'));
         }
 
         // private functions
 
-        function handleSuccess(res) {
+        function handleSuccess(res)
+        {
             return res.data;
         }
 
-        function handleError(error) {
-            return function () {
+        function handleError(error)
+        {
+            return function ()
+            {
                 return { success: false, message: error };
             };
         }
